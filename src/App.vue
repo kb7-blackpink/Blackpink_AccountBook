3027cd363aa8a982c6c433719d196e8bf90f9492
KHJ <jin3574@dgu.ac.kr>
2026-04-08 14:24:13 +0900

    feat: splash-screen 추가
    /router/index.js에 SplashPage 경로 추가
    /pages/SlpashPage.vue 생성
    /pages/HomePage.vue 생성(임시)
    /App.vue에 router-view 태그 추가
    /main.css에 폰트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup></script>
-
 <template>
-  <h1 class="text-3xl font-bold text-blue-600">Tailwind 적용 확인</h1>
+  <router-view />
 </template>
+
+<script setup></script>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import SplashPage from '@/pages/SplashPage.vue';
+
+// App 켜지자마자 SplashPage 보여줌
+const routes = [
+  {
+    path: '/',
+    name: 'splash',
+    component: SplashPage,
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('@/pages/HomePage.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: routes,
+});
 
-export default router
+export default router;
