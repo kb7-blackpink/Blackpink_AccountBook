cd77d42d693d4409b550d4af119a2396cd5495d4
hyeonjin6530 <cola1876@naver.com>
2026-04-08 15:41:57 +0900

    fix: error 해결

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
 
 export const useUserStore = defineStore('user', () => {
   const mode = ref('lucky'); // 'lucky' | 'unlucky'
