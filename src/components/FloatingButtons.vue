54c3af397ee148e77b7fed639a203f5af022e9f3
dodo5517 <kh9015548@gmail.com>
2026-04-10 10:53:19 +0900

    style: CTA 버튼 크기 및 색상 수정

--- src/components/FloatingButtons.vue
+++ src/components/FloatingButtons.vue
@@ -4,25 +4,25 @@
   >
     <button
       @click="modalStore.openAddModal()"
-      class="w-7 h-7 lg:w-10 lg:h-10 pb-0.5 font-bold border rounded-full text-xl lg:text-2xl flex items-center justify-center cursor-pointer"
+      class="w-12 h-12 lg:w-12 lg:h-12 pb-0.5 font-bold border rounded-full text-xl lg:text-2xl flex items-center justify-center cursor-pointer"
       :class="[
         userStore.mode === 'lucky'
-          ? 'border-app bg-app text-green-500/70'
-          : 'border-white/30 bg-white/10 text-white ',
+          ? 'border-app text-white bg-[#C0E068]'
+          : 'border-white/30 text-white bg-[#7c3aed]',
       ]"
     >
-      +
+      <Plus stroke-width="5" />
     </button>
     <button
       @click="scrollTop"
-      class="w-7 h-7 lg:w-10 lg:h-10 lg:text-lg text-xs border rounded-full cursor-pointer"
+      class="flex items-center justify-center w-12 h-12 lg:w-12 lg:h-12 lg:text-lg text-xs border rounded-full cursor-pointer"
       :class="[
         userStore.mode === 'lucky'
-          ? 'border-app bg-app text-neutral-400'
-          : 'border-white/30  bg-white/10 text-neutral-200',
+          ? 'border-app text-app bg-neutral-100'
+          : 'border-white/30  text-white bg-white/10',
       ]"
     >
-      ⬆
+      <ArrowBigUp stroke-width="2" />
     </button>
   </div>
 </template>
@@ -32,6 +32,7 @@ import { useBudgetStore } from '@/stores/budget';
 import { onMounted } from 'vue';
 import { useUserStore } from '@/stores/user';
 import { useModalStore } from '@/stores/modal';
+import { ArrowBigUp, Plus } from 'lucide-vue-next';
 
 const budgetStore = useBudgetStore();
 const userStore = useUserStore();
